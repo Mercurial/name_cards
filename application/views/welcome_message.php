e03397ca5ddb602034f76cf2e3665ae71f2dc9b7
unknown <Daniel@daniel8.(none)>
2012-06-17 01:19:21 +0800

    -

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -16,28 +16,51 @@ body{
 	width:100%;
 	height:78px;
 }
+	#main-header{
+		background:#999;
+		width:1030px;
+		height:78px;
+		margin:0 auto;
+	}
 #menu{
 	background:#cb3232;
 	width:100%;
 	height:24px;
 }
+	#main-menu{
+		background:#333;
+		width:1030px;
+		height:24px;
+		margin:0 auto;
+	}
 #content{
-	background:#ccc;
+	background:#999;
 	width:100%;
 	min-height:700px;
+}
+#footer{
+	background:#555;
+	width:100%;
+	height:100px;
 }
 </style>
 </head>
 <body>
 <div id="header">
+	<div id="main-header">
 	
+	</div>
 </div>
 <div id="menu">
+	<div id="main-menu">
 	
+	</div>
 </div>
+
 <div id="content">
-	
+
 </div>
+
 <div id="footer">
 	
 </div>
